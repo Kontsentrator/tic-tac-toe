775328fc0703bdd7ff9cee492d946f99871a48e6
Dmitriy <dima.dima-73@yandex.ru>
2021-09-27 10:30:21 +0300

    Create cell.tsx

---
+++ pages/cell.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+type SquareProps = {
+    value: string,
+    onClick(e: React.MouseEvent<HTMLElement>): void
+}
+
+const Cell: React.FC<SquareProps> = ({value, onClick}) => {
+    return(
+        <div className="row__cell" onClick={(e) => onClick(e)}>
+            {value}
+        </div>
+    );
+}
+
+export default Cell;
