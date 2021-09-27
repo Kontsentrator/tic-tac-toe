10672497c1ca9b3bbd2553f8aa046350f7fd9639
Dmitriy <dima.dima-73@yandex.ru>
2021-09-27 19:00:00 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,9 @@ import React, { useState } from 'react';
 import TicTacToe from './game'
 
 export default function Home() {
-  const [board, setBoard] = useState<string[]>(Array(9).fill(null));
-
   return (
     <div className="game__wrap">
-      <TicTacToe board={board} />
+      <TicTacToe />
     </div>
   )
 }
