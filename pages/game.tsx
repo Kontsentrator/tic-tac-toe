3a220bc3b36e0fab93e3fa35ac7dd2b27beb296b
Dmitriy <dima.dima-73@yandex.ru>
2021-09-27 18:59:58 +0300

    Update game.tsx

--- pages/game.tsx
+++ pages/game.tsx
@@ -2,22 +2,18 @@ import React, { useState } from "react";
 import Cell from "./cell";
 
 type TicTacToeProps = {
-    board: string[]
+    
 }
 
-const TicTacToe: React.FC<TicTacToeProps> = ({board}) => {
+const TicTacToe: React.FC<TicTacToeProps> = () => {
+    const [board, setBoard] = useState<string[]>(Array(9).fill(null));
     const [nextTurn, setNextTurn] = useState<Boolean>(true);
 
     const handleCellClick = (e: React.MouseEvent<HTMLElement>) => { 
-        if((e.target as HTMLElement).innerHTML === '') {
-            if(nextTurn)
-                (e.target as HTMLElement).innerHTML = 'X';
-            else {
-                (e.target as HTMLElement).innerHTML = 'O';
-            }
-            setNextTurn(prev => !prev);
-            localStorage.setItem('nextTurn', nextTurn + '');
-        }
+        console.log(e);
+        setNextTurn(prev => !prev);
+        localStorage.setItem('nextTurn', nextTurn + '');
+        
     }
 
     return(
