0caa3b913eec66894589e501a8cc11c6cf3e2e62
Dmitriy <dima.dima-73@yandex.ru>
2021-09-27 10:51:01 +0300

    Update game.tsx

--- pages/game.tsx
+++ pages/game.tsx
@@ -1,25 +1,31 @@
-import React, { EventHandler, ReactElement, useState } from "react";
+import React, { useState } from "react";
 import Cell from "./cell";
 
 type TicTacToeProps = {
-    field: string[]
+    board: string[]
 }
 
-const TicTacToe: React.FC<TicTacToeProps> = ({field}) => {
+const TicTacToe: React.FC<TicTacToeProps> = ({board}) => {
     const [nextTurn, setNextTurn] = useState<Boolean>(true);
 
     const handleCellClick = (e: React.MouseEvent<HTMLElement>) => { 
-        (e.target as HTMLElement).innerHTML = "turn as string";
+        if(nextTurn)
+            (e.target as HTMLElement).innerHTML = 'X';
+        else {
+            (e.target as HTMLElement).innerHTML = 'O';
+        }
         setNextTurn(prev => !prev);
     }
 
     return(
-        <div className="field">  
-        {
-            field.map((cell, index) => {
-                <Cell key={index} onClick={handleCellClick} value={cell} />
-            })
-        }
+        <div className="field">
+            {board.map((value, index) =>
+                <Cell 
+                    key={index} 
+                    onClick={handleCellClick} 
+                    value={value} 
+                />
+            )}
         </div>
     );
 }
