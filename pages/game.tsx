dffe4711aebd5112197e6da36d56dccd7df8d268
Dmitriy <dima.dima-73@yandex.ru>
2021-09-28 10:56:21 +0300

    Update game.tsx

--- pages/game.tsx
+++ pages/game.tsx
@@ -8,6 +8,22 @@ type TicTacToeProps = {
 const TicTacToe: React.FC<TicTacToeProps> = () => {
     const [board, setBoard] = useState<string[]>(Array(9).fill('')); // Поле игры
     const [playerNextTurn, setPlayerNextTurn] = useState<Boolean>(true); // Чей ход следующий
+    const [restart, setRestart] = useState<boolean>(true);
+
+    // -------------- Эффекты -------------
+    // Проверка, что на поле остались пустые клетки
+    const hasEmptyCells = (array: string[]): boolean => {
+        return !array.every(item => item !== '');
+    }
+    useEffect(() => {
+        console.log(hasEmptyCells(board));
+    }, [hasEmptyCells]);
+
+    // Перезапуск игры
+    useEffect(() => {
+        if(restart)
+            restartGame();
+    }, []);
 
     // Загрузка значений из localstorage
     useEffect(() => {
@@ -28,24 +44,56 @@ const TicTacToe: React.FC<TicTacToeProps> = () => {
         localStorage.setItem('playerNextTurn', JSON.stringify(playerNextTurn));
     }, [playerNextTurn]);
 
+    // Ход бота
+    useEffect(() => {
+        if(!playerNextTurn) {
+            let tempBoard = board;
+            let rand;
+            do {
+                rand = Math.round(random(0, 9));
+            } while(tempBoard[rand] !== '');
+            tempBoard[rand] = 'o';
+            setBoard(tempBoard);
+            setPlayerNextTurn(prev => !prev);
+        }
+    }, [playerNextTurn]);
+
+    // -------------- Методы -------------
+
+    const random = (min: number, max: number): number => {
+        return min + (Math.random() * (max-min));
+    }
+
+    
+
+    const restartGame = () => {
+        setBoard(Array(9).fill(''));
+        setPlayerNextTurn(true);
+    }
+
+    // Обработка клика по клетке поля
     const handleCellClick = (id: number) => { 
-        console.log("Зашли");
         let tempBoard = board;
-        playerNextTurn ? tempBoard[id] = 'x' : tempBoard[id] = 'o';
+        if(playerNextTurn)
+            tempBoard[id] = 'x';
 
         setBoard(tempBoard);
         setPlayerNextTurn(prev => !prev);
     }
 
     return(
-        <div className="field">
-            {board.map((value, index) =>
-                <Cell 
-                    key={index} 
-                    onClick={() => handleCellClick(index)} 
-                    value={value} 
-                />
-            )}
+        <div className="game">
+            <div className="field">
+                {board.map((value, index) =>
+                    <Cell 
+                        key={index} 
+                        onClick={() => handleCellClick(index)} 
+                        value={value} 
+                    />
+                )}
+            </div>
+            <button className="button__restart" onClick={restartGame}>Начать заново</button>
+            <span>{hasEmptyCells(board) ? '' : 'Игра окончена'}</span>
         </div>
     );
 }
