867a42008195b148d6d020b01b4ef483855d2428
Dmitriy <dima.dima-73@yandex.ru>
2021-09-28 10:04:52 +0300

    Update game.tsx

--- pages/game.tsx
+++ pages/game.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Cell from "./cell";
 
 type TicTacToeProps = {
@@ -6,14 +6,35 @@ type TicTacToeProps = {
 }
 
 const TicTacToe: React.FC<TicTacToeProps> = () => {
-    const [board, setBoard] = useState<string[]>(Array(9).fill(null));
-    const [nextTurn, setNextTurn] = useState<Boolean>(true);
+    const [board, setBoard] = useState<string[]>(Array(9).fill('')); // Поле игры
+    const [playerNextTurn, setPlayerNextTurn] = useState<Boolean>(true); // Чей ход следующий
 
-    const handleCellClick = (e: React.MouseEvent<HTMLElement>) => { 
-        console.log(e);
-        setNextTurn(prev => !prev);
-        localStorage.setItem('nextTurn', nextTurn + '');
+    // Загрузка значений из localstorage
+    useEffect(() => {
+        if(localStorage.getItem('board'))
+            setBoard(JSON.parse(localStorage.getItem('board') as string));
         
+        if(localStorage.getItem('playerNextTurn'))
+            setPlayerNextTurn(JSON.parse(localStorage.getItem('playerNextTurn') as string));
+    }, []);
+
+    // Установка поля игры в localhost
+    useEffect(() => {
+        localStorage.setItem('board', JSON.stringify(board));
+    }, [board.values(), board]);
+
+    // Установка значения, кто ходит следующим
+    useEffect(() => {
+        localStorage.setItem('playerNextTurn', JSON.stringify(playerNextTurn));
+    }, [playerNextTurn]);
+
+    const handleCellClick = (id: number) => { 
+        console.log("Зашли");
+        let tempBoard = board;
+        playerNextTurn ? tempBoard[id] = 'x' : tempBoard[id] = 'o';
+
+        setBoard(tempBoard);
+        setPlayerNextTurn(prev => !prev);
     }
 
     return(
@@ -21,7 +42,7 @@ const TicTacToe: React.FC<TicTacToeProps> = () => {
             {board.map((value, index) =>
                 <Cell 
                     key={index} 
-                    onClick={handleCellClick} 
+                    onClick={() => handleCellClick(index)} 
                     value={value} 
                 />
             )}
