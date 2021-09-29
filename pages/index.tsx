0ca62d7e2491ba2bfcd1c2e1e019eeccf605bd0f
Dmitriy <dima.dima-73@yandex.ru>
2021-09-29 12:42:46 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,25 @@
 import React, { useState } from 'react';
-import TicTacToe from './game'
+import TicTacToe from './game';
+import { createStore } from 'redux';
+import { PayloadAction } from 'redux';
+
+const defaultState = {
+  cash: 0,
+}
+
+const reducer = (state = defaultState, action: PayloadAction) => {
+  switch(action.type) {
+    case "ADD_CASH":
+      return {...state, cash: state.cash + action.payload}
+    case "GET_CASH":
+      return {...state, cash: state.cash - action.payload}
+
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
 
 export default function Home() {
   return (
