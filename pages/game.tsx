4f55f47f624fd9e0bf610dec504f658f2fc5fe10
Dmitriy <dima.dima-73@yandex.ru>
2021-10-03 15:04:52 +0300

    Update game.tsx

--- pages/game.tsx
+++ pages/game.tsx
@@ -2,25 +2,17 @@ import React, { useState, useEffect } from "react";
 import Cell from "./cell";
 
 import { useAppSelector, useAppDispatch } from './store/hooks';
-import { move } from './store/boardSlice';
+import { move, restart } from './store/boardSlice';
 
-type TicTacToeProps = {
-    playerFirst: boolean
-}
-
-const TicTacToe: React.FC<TicTacToeProps> = ({playerFirst}) => {
-    const [board, setBoard] = useState<string[]>(Array(9).fill('')); // Поле игры
-    const [playerNextTurn, setPlayerNextTurn] = useState<Boolean>(playerFirst); // Чей ход следующий
-    const [restart, setRestart] = useState<boolean>(false);
-
-    const board1 = useAppSelector(state => state.myReducer.board);
+const TicTacToe: React.FC = () => {
+    const board = useAppSelector(state => state.myReducer.board);
+    const nextTurn = useAppSelector(state => state.myReducer.nextTurn);
     const dispatch = useAppDispatch();
 
     const setMove = (flag: string, pos: number) => {
         dispatch({type: move.type, payload: {flag: flag, pos: pos}});
     }
     
-
     // const stream$ = new Observable(observer => {
     //     observer.next(board);
     // });
@@ -32,44 +24,21 @@ const TicTacToe: React.FC<TicTacToeProps> = ({playerFirst}) => {
 
     // Перезапуск игры
     useEffect(() => {
-        if(restart) {
+        //if(restart) {
             restartGame();
-        }
+        //}
     }, []);
 
     // Ход бота
     useEffect(() => {
-        if(!playerNextTurn && hasEmptyCells(board)) {
-            let tempBoard = board;
+        if(!nextTurn && hasEmptyCells(board)) {
             let rand;
             do {
                 rand = Math.round(random(0, 9));
-            } while(tempBoard[rand] !== '');
+            } while(board[rand] !== '');
             setMove("o", rand);
-            tempBoard[rand] = 'o';
-            setBoard(tempBoard);
-            setPlayerNextTurn(prev => !prev);
         }
-    }, [playerNextTurn]);
-
-    // Установка поля игры в localhost
-    useEffect(() => {
-        localStorage.setItem('board', JSON.stringify(board));
-    }, [board.values(), board]);
-
-    // Установка значения, кто ходит следующим
-    useEffect(() => {
-        localStorage.setItem('playerNextTurn', JSON.stringify(playerNextTurn));
-    }, [playerNextTurn]);
-
-    // Загрузка значений из localstorage
-    useEffect(() => {
-        if(localStorage.getItem('board'))
-            setBoard(JSON.parse(localStorage.getItem('board') as string));
-        
-        if(localStorage.getItem('playerNextTurn'))
-            setPlayerNextTurn(JSON.parse(localStorage.getItem('playerNextTurn') as string));
-    }, []);
+    }, [nextTurn]);
 
     // -------------- Методы -------------
 
@@ -85,29 +54,21 @@ const TicTacToe: React.FC<TicTacToeProps> = ({playerFirst}) => {
 
     // Перезапуск игры
     const restartGame = () => {
-        console.log("Перезапуск");
-        localStorage.clear();
-        setBoard(Array(9).fill(''));
-        setPlayerNextTurn(playerFirst);
+        dispatch({type: restart.type});
     }
 
     // Обработка клика по клетке поля
     const handleCellClick = (id: number) => { 
-        let tempBoard = board;
-        if(playerNextTurn && tempBoard[id] === '') {
-            // tempBoard[id] = 'x';
-            // setBoard(tempBoard);
-
+        if(nextTurn && board[id] === '') {
             setMove("x", id);
-            setPlayerNextTurn(prev => !prev);
         }
     }
 
     return(
         <div className="game">
-            <p>{playerFirst ? "Вы ходите первым" : "Вы ходите вторым"}</p>
+            <p>{nextTurn ? "Вы ходите первым" : "Вы ходите вторым"}</p>
             <div className="field">
-                {board1.map((value, index) =>
+                {board.map((value, index) =>
                     <Cell 
                         key={index} 
                         onClick={() => handleCellClick(index)} 
