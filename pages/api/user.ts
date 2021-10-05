7794d14595d28afc5602ef8673c5f173c3201140
Dmitriy <dima.dima-73@yandex.ru>
2021-10-05 18:00:41 +0300

    Delete user.ts

--- pages/api/user.ts
+++
@@ -1,5 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next"
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.json([{ id: 1, status: 'OK' }, {id: 2, status: 'OKAY'}])
-}
