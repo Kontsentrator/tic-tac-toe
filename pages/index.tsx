f79d0d1b36f99c9270d8b60daec8aa9b371cc1d3
Dmitriy <dima.dima-73@yandex.ru>
2021-10-05 18:00:51 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
-import TicTacToe from './game';
+import TicTacToe from './components/tictactoe';
 import { store } from './store/store';
+import { GetStaticProps } from 'next';
+import { Datas } from './interfaces/interface';
 
-export default function Home() {
+export const getStaticProps: GetStaticProps = async ctx => {
+  const response = await fetch("http://localhost:3000/api/board");
+  const data = await response.json();
+  console.log(data);
+  return {props: {data}};
+}
+
+export default function Home({data}: Datas) {
   return (
     <div className="game__wrap">
       <Provider store={store}>
-        <TicTacToe />
+        <TicTacToe data={data} />
       </Provider>
     </div>
   )
