aa9076fa1c8ec5d44d495e11ceb97ba8747abdcd
Dmitriy <dima.dima-73@yandex.ru>
2021-10-06 08:16:24 +0300

    Update board.ts

--- pages/api/board.ts
+++ pages/api/board.ts
@@ -1,18 +1,21 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import { initialState } from "../store/boardSlice";
 
-const board = initialState.board;
-const nextTurn = initialState.nextTurn;
+
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if(req.method === 'GET') {
+    const board = initialState.board;
+    const nextTurn = initialState.nextTurn;
     res.status(200).json([{ id: 1, board: board, nextTurn: nextTurn }]);
   } else if (req.method === 'POST') {
-    const board = req.body.board;
+    const newBoard = req.body.board;
+    const newNextTurn = req.body.nextTurn;
+
     const newData = {
       id: Date.now(),
-      board: board,
-      nextTurn: true
+      board: newBoard,
+      nextTurn: newNextTurn
     }
     res.status(201).json(newData);
   }
