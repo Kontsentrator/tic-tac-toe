0b28e853af5ef1addaa354075ff34e7a806fda0d
Dmitriy <dima.dima-73@yandex.ru>
2021-10-07 14:16:43 +0300

    Update boardSlice.ts

--- pages/store/boardSlice.ts
+++ pages/store/boardSlice.ts
@@ -16,16 +16,14 @@ export const boardSlice = createSlice({
     name: "board",
     initialState,
     reducers: {
-        move: (state, action: PayloadAction<{flag: string, pos: number[]}>) => {
-            let col = action.payload.pos[0];
-            let row = action.payload.pos[1];
-            state.board[col][row] = action.payload.flag;
+        move: (state, action: PayloadAction<{flag: string, row: number, col: number}>) => {
+            state.board[action.payload.row][action.payload.col] = action.payload.flag;
             state.nextTurn = !state.nextTurn;
         },
         restart: (state) => {
             state.board = initialState.board;
             state.nextTurn = initialState.nextTurn;
-        }
+        },
     }
 });
 
