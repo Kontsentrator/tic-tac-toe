980a4c1c6d1bb50304c2a72d6ed7df518229065c
Dmitriy <dima.dima-73@yandex.ru>
2021-10-11 08:04:09 +0300

    Update boardSlice.ts

--- pages/store/boardSlice.ts
+++ pages/store/boardSlice.ts
@@ -1,30 +1,35 @@
-import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 interface IBoardState {
-    board: Array<string[]>,
-    nextTurn: boolean,
+  board: Array<string[]>;
+  nextTurn: boolean;
 }
 
 export const initialState: IBoardState = {
-    board: [['', '', ''],
-            ['', '', ''],
-            ['', '', '']],
-    nextTurn: true,
-}
+  board: [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ],
+  nextTurn: true,
+};
 
 export const boardSlice = createSlice({
-    name: "board",
-    initialState,
-    reducers: {
-        move: (state, action: PayloadAction<{flag: string, row: number, col: number}>) => {
-            state.board[action.payload.row][action.payload.col] = action.payload.flag;
-            state.nextTurn = !state.nextTurn;
-        },
-        restart: (state) => {
-            state.board = initialState.board;
-            state.nextTurn = initialState.nextTurn;
-        }
-    }
+  name: "board",
+  initialState,
+  reducers: {
+    move: (
+      state,
+      action: PayloadAction<{ flag: string; row: number; col: number }>
+    ) => {
+      state.board[action.payload.row][action.payload.col] = action.payload.flag;
+      state.nextTurn = !state.nextTurn;
+    },
+    restart: (state) => {
+      state.board = initialState.board;
+      state.nextTurn = initialState.nextTurn;
+    },
+  },
 });
 
 export const { move, restart } = boardSlice.actions;
