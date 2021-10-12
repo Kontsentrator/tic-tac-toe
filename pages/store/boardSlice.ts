0bb0466640e6159f78a1267da37651c9d3495198
Dmitriy <dima.dima-73@yandex.ru>
2021-10-12 10:23:04 +0300

    Update boardSlice.ts

--- pages/store/boardSlice.ts
+++ pages/store/boardSlice.ts
@@ -1,18 +1,30 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
-interface IBoardState {
-  board: Array<string[]>;
-  nextTurn: boolean;
+interface IBoardSize {
+  rowsNum: number,
+  colsNum: number,
 }
 
-export const boardSizeState = {
-  rowsNum: 4,
-  colsNum: 3
-};
+interface IBoardState {
+  board: Array<string[]>,
+  nextTurn: boolean,
+  boardSize: IBoardSize,
+  winner: string,
+}
+
+const boardSizeState: IBoardSize = {
+  rowsNum: 3,
+  colsNum: 3,
+}
 
 export const initialState: IBoardState = {
   board: Array(boardSizeState.rowsNum).fill(Array(boardSizeState.colsNum).fill('')),
   nextTurn: true,
+  boardSize: {
+    rowsNum: boardSizeState.rowsNum,
+    colsNum: boardSizeState.colsNum,
+  },
+  winner: "",
 };
 
 export const boardSlice = createSlice({
@@ -29,9 +41,13 @@ export const boardSlice = createSlice({
     restart: (state) => {
       state.board = initialState.board;
       state.nextTurn = initialState.nextTurn;
+      state.winner = initialState.winner;
     },
+    setWinner: (state, action: PayloadAction<string>) => {
+      state.winner = action.payload;
+    }
   },
 });
 
-export const { move, restart } = boardSlice.actions;
+export const { move, restart, setWinner } = boardSlice.actions;
 export default boardSlice.reducer;
