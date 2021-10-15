4ae51bd2a5b1705fdd63a7a3f47211265a39bdd2
Dmitriy <dima.dima-73@yandex.ru>
2021-10-15 14:40:36 +0300

    Update interface.ts

--- pages/interfaces/interface.ts
+++ pages/interfaces/interface.ts
@@ -1,16 +1,11 @@
-export interface IStaticProps {
-  id: number;
-  status: string;
-}
-
-export interface Datas {
-  data: IStaticProps[];
-}
-
 // Статистика по игре
 export interface IMoveInfo {
   game: number;
   row: number;
   col: number;
   isPlayer: boolean;
+}
+
+export interface IMovesInfo {
+  data: IMoveInfo[]
 }
