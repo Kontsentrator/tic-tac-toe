799c5cad499b3610562e3f11562f86418f9c4f30
Dmitriy <dima.dima-73@yandex.ru>
2021-10-20 11:08:25 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Provider } from "react-redux";
 import TicTacToe from "./components/tictactoe";
 import { store } from "./store/store";
 import { GetStaticProps } from "next";
 import { IMovesInfo } from "./interfaces/interface";
 
+
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const response = await fetch("http://localhost:3000/api/board");
-  const data = await response.json();
-  console.log(data);
-  return { props: { data } };
+  const moves = await response.json();
+  return { props: { moves } };
 };
 
 //const StatisticContext = React.createContext();
 
-export default function Home({ data }: IMovesInfo) {
+export default function Home({ moves }: IMovesInfo) {
   return (
     <div className="game__wrap">
       <Provider store={store}>
-        <TicTacToe data={data} />
+        <TicTacToe moves={moves} />
       </Provider>
     </div>
   );
