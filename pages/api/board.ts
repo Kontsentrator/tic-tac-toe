78abcda4543d4fe345600883fcc6784d23d65bf4
Dmitriy <dima.dima-73@yandex.ru>
2021-10-28 10:50:36 +0300

    Update board.ts

--- pages/api/board.ts
+++ pages/api/board.ts
@@ -5,7 +5,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
     res.status(200).json(history);
   } else if (req.method === "POST") {
-    const newMoveInfo = req.body.currentMoveInfo;
+    const newMoveInfo = req.body.moveInfo;
     history.push(newMoveInfo);
     res.status(201).json(history);
   }
