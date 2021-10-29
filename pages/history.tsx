94fbb3ffdd841bc1251fb0c010cacb01dac8bc0f
Dmitriy <dima.dima-73@yandex.ru>
2021-10-29 15:09:39 +0300

    Update history.tsx

--- pages/history.tsx
+++ pages/history.tsx
@@ -1,31 +1,42 @@
 import React, { useState } from "react";
 import { GetStaticProps } from "next";
-import { IMovesInfo } from "./interfaces/interface";
+import { IStatistic } from "./interfaces/interface";
 
 const API_ENDPOINT = "http://localhost:3000/api";
 const RESOURCE = "/board";
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-  const moves = await fetch(API_ENDPOINT + RESOURCE)
+  const statistic = await fetch(API_ENDPOINT + RESOURCE)
     .then((res) => res.json())
     .catch((error) => {
       console.log(error);
       return [];
     });
-  return { props: { moves } };
+  return { props: statistic };
 };
 
-export default function History({ moves }: IMovesInfo) {
-    console.log(moves);
+export default function History(statistic: IStatistic) {
   return (
     <div className="history">
-      {moves.map((move, index) => (
-          <div key={index} className="history__move">
-              <div className="move__param">Игра № {move.game}</div>
-              <div className="move__param">Ряд: {move.row}</div>
-              <div className="move__param">Колонка: {move.col}</div>
-              <div className="move__param">Ход игрока: {move.isPlayer ? "Да" : "Нет"}</div>
-          </div>
+      {statistic.history.map((game, index) => (
+        <div key={index} className="history__game">
+          <p className="history__game-title">Игра № {index + 1}</p>
+
+          {game.map((move, index) => (
+            <div
+              key={index}
+              className={`history__move ${
+                move.isPlayer ? "history__move_player" : "history__move_bot"
+              }`}
+            >
+              <p className="move__param">Ряд: {move.row}</p>
+              <p className="move__param">Колонка: {move.col}</p>
+              <p className="move__param">
+                Игрок: {move.isPlayer ? "да" : "нет"}
+              </p>
+            </div>
+          ))}
+        </div>
       ))}
     </div>
   );
