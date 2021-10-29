4f9a6b39511bfa1e5a4baf39b0354eed0c8d5010
Dmitriy <dima.dima-73@yandex.ru>
2021-10-29 15:09:29 +0300

    Update board.ts

--- pages/api/board.ts
+++ pages/api/board.ts
@@ -1,12 +1,16 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { history } from "../data/statistic";
+import { statistic } from "../data/statistic";
+import { IStatistic } from "../interfaces/interface";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
-    res.status(200).json(history);
+    console.log("GET", statistic);
+    res.status(200).json(statistic);
   } else if (req.method === "POST") {
-    const newMoveInfo = req.body.moveInfo;
-    history.push(newMoveInfo);
-    res.status(201).json(history);
+    const newStatistic: IStatistic = req.body.stat;
+    statistic.botWinCount = newStatistic.botWinCount;
+    statistic.playerWinCount = newStatistic.playerWinCount;
+    statistic.history = newStatistic.history;
+    res.status(201).json(newStatistic);
   }
 }
