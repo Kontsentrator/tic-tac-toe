caf6b080a626463ddc2381fc09317681871bd447
Dmitriy <dima.dima-73@yandex.ru>
2021-10-29 15:09:42 +0300

    Update interface.ts

--- pages/interfaces/interface.ts
+++ pages/interfaces/interface.ts
@@ -1,17 +1,13 @@
 // Статистика по игре
-export interface IMoveInfo {
-  game: number,
-  row: number,
-  col: number,
-  isPlayer: boolean,
-}
 
-export interface IMovesInfo {
-  moves: IMoveInfo[],
+export interface IMoveInfo {
+  row: number;
+  col: number;
+  isPlayer: boolean;
 }
 
 export interface IStatistic {
-  playerWinCount: number,
-  botWinCount: number,
-  history: IMoveInfo[],
+  history: IMoveInfo[][];
+  botWinCount: number;
+  playerWinCount: number;
 }
