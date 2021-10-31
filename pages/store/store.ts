f1307529ac96d0d8e5d055918468822479d364ba
olejech <olejech@gmail.com>
2021-10-31 21:50:34 +0300

    add redux-observable

--- pages/store/store.ts
+++ pages/store/store.ts
@@ -1,11 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { boardSlice } from "./boardSlice";
+import { combineEpics } from "redux-observable";
+import { boardSlice, myMoveEpic } from "./boardSlice";
+import { createEpicMiddleware } from 'redux-observable';
+
+const epicMiddleware = createEpicMiddleware();
 
 export const store = configureStore({
   reducer: {
     boardReducer: boardSlice.reducer,
   },
+  middleware: [
+    epicMiddleware
+  ]
 });
+
+export const rootEpic = combineEpics(
+  myMoveEpic,
+);
+
+epicMiddleware.run(rootEpic)
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
