0a6302f6e543153074f3383af048be95e60ac8b1
Dmitriy <dima.dima-73@yandex.ru>
2021-11-24 14:43:36 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,22 @@
 import React from "react";
 import { Provider } from "react-redux";
-import TicTacToe from "./components/tictactoe";
+import TicTacToe from "./components/Tictactoe";
 import { store } from "./store/store";
-import { GetStaticProps } from "next";
 import { statistic } from "./data/statistic";
 import { WinCountContext } from "./data/context";
-
-const API_ENDPOINT = "http://localhost:3000/api";
-const RESOURCE = "/board";
 
 export default function Home() {
   console.log(statistic);
   const winCount = {
     player: statistic.playerWinCount,
     bot: statistic.botWinCount,
-  }
+  };
 
   return (
-    <div className="game__wrap">
-      <Provider store={store}>
-        <WinCountContext.Provider value={winCount}>
-          <TicTacToe />
-        </WinCountContext.Provider>
-      </Provider>
-    </div>
+    <Provider store={store}>
+      <WinCountContext.Provider value={winCount}>
+        <TicTacToe />
+      </WinCountContext.Provider>
+    </Provider>
   );
 }
