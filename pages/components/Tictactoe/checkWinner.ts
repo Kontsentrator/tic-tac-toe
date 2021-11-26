02f83c127f29f0efef34f7efbd91d976275ada60
Dmitriy <dima.dima-73@yandex.ru>
2021-11-26 16:04:52 +0300

    Update checkWinner.ts

--- pages/components/Tictactoe/checkWinner.ts
+++ pages/components/Tictactoe/checkWinner.ts
@@ -1,13 +1,14 @@
-
-
 export const checkWinner = (flag: string, board: string[][]) => {
-  if ((checkLines(flag) || checkDiagonals(flag)) && !winner) {
+  if (checkLines(flag, board) || checkDiagonals(flag, board)) {
     return true;
   }
   return false;
 };
 
-const checkLines = (flag: string): boolean => {
+const checkLines = (flag: string, board: string[][]): boolean => {
+  let rowsCount = board.length,
+    colsCount = board[0].length;
+
   for (let row = 0; row < rowsCount; row++) {
     let rowCheck = true,
       colCheck = true;
@@ -23,7 +24,9 @@ const checkLines = (flag: string): boolean => {
   return false;
 };
 
-const checkDiagonals = (flag: string): boolean => {
+const checkDiagonals = (flag: string, board: string[][]): boolean => {
+  let rowsCount = board.length;
+
   let toRight = true,
     toLeft = true;
   for (let row = 0; row < rowsCount; row++) {
