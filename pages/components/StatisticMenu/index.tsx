d64dc700f5c0f6752f411a0db7f920db0e273684
Dmitriy <dima.dima-73@yandex.ru>
2021-11-26 11:30:55 +0300

    Update index.tsx

--- pages/components/StatisticMenu/index.tsx
+++ pages/components/StatisticMenu/index.tsx
@@ -4,15 +4,15 @@ import { WinCountContext } from "../../data/context";
 import styles from "./statisticMenu.module.scss";
 
 interface statisticMenuProps {
-  className: string;
+  className?: string;
 }
 
 const StatisticMenu: React.FC<statisticMenuProps> = ({ className }) => {
-  const contextWinCount = useContext(WinCountContext);
+  const {statistic} = useContext(WinCountContext);
   return (
     <div className={cn(className, styles.statisticMenu)}>
-      <p>Игрок: {contextWinCount.player}</p>
-      <p>Бот: {contextWinCount.bot}</p>
+      <p>Игрок: {statistic.player}</p>
+      <p>Бот: {statistic.bot}</p>
     </div>
   );
 };
