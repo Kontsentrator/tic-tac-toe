87e5e3b2a09575ee9af0652f7785136215b75123
Dmitriy <dima.dima-73@yandex.ru>
2021-11-26 16:04:58 +0300

    Update history.tsx

--- pages/history.tsx
+++ pages/history.tsx
@@ -6,48 +6,51 @@ import styles from "./history.module.scss";
 
 interface IHistoryProps {
   statistic: IStatistic;
+  error?: string;
 }
 
 const API_ENDPOINT = "http://localhost:3000/api";
 const RESOURCE = "/board";
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-  const statistic = await fetch(API_ENDPOINT + RESOURCE)
-    .then((res) => res.json())
-    .catch((error) => {
-      console.log(error);
-    });
-  return { props: { statistic } };
+  let error: Error = Error();
+  const statistic: IStatistic = await fetch(API_ENDPOINT + RESOURCE).then(
+    (res) => res.json().catch((err) => {error = err; return null})
+  );
+  return { props: { statistic: statistic, error: error.message } };
 };
 
-const History: React.FC<IHistoryProps> = ({ statistic }) => {
+const History: React.FC<IHistoryProps> = ({ statistic, error }) => {
   return (
     <div className={styles.history}>
+      {error && <div className={styles.history__error}>{error}</div>}
       {statistic &&
-        statistic.history.map((game, index) => (
+        statistic.history.map(
+          (game, index) =>
+            game && (
+              <div key={index} className={styles.history__game}>
+                <p className={styles.history__gameTitle}>Игра № {index + 1}</p>
 
-        game && (<div key={index} className={styles.history__game}>
-            <p className={styles.history__gameTitle}>Игра № {index + 1}</p>
-
-            {game.map((move, index) => (
-              <div
-                key={index}
-                className={cn(
-                  styles.history__move,
-                  move.isPlayer
-                    ? styles.history__movePlayer
-                    : styles.history__moveBot
-                )}
-              >
-                <p className={styles.history__param}>Ряд: {move.row}</p>
-                <p className={styles.history__param}>Колонка: {move.col}</p>
-                <p className={styles.history__param}>
-                  Игрок: {move.isPlayer ? "да" : "нет"}
-                </p>
+                {game.map((move, index) => (
+                  <div
+                    key={index}
+                    className={cn(
+                      styles.history__move,
+                      move.isPlayer
+                        ? styles.history__movePlayer
+                        : styles.history__moveBot
+                    )}
+                  >
+                    <p className={styles.history__param}>Ряд: {move.row}</p>
+                    <p className={styles.history__param}>Колонка: {move.col}</p>
+                    <p className={styles.history__param}>
+                      Игрок: {move.isPlayer ? "да" : "нет"}
+                    </p>
+                  </div>
+                ))}
               </div>
-            ))}
-          </div>)
-        ))}
+            )
+        )}
     </div>
   );
 };
